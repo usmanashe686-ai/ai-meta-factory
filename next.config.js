1805e369a2fac6f940e9a2e63da5869ceb115d76
Usman Ashe <usmanashe686@gmail.com>
2026-01-23 15:05:22 +0300

    fix: enable static export for Firebase

--- next.config.js
+++ next.config.js
@@ -1,24 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  swcMinify: false,       // disable SWC
-  experimental: {
-    forceSwcTransforms: false,
-  },
-  webpack: (config) => {
-    // replace SWC loader with Babel loader
-    config.module.rules.forEach(rule => {
-      if (rule.use && rule.use.loader && rule.use.loader.includes('next-swc-loader')) {
-        rule.use.loader = 'babel-loader';
-        rule.use.options = {
-          presets: ['next/babel'],
-        };
-      }
-    });
-    return config;
+  images: {
+    unoptimized: true,
   },
 };
 
