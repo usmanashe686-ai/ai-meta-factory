c21fd3323968630dd71c445928d4076cae9aed1c
Usman Ashe <usmanashe686@gmail.com>
2026-01-23 16:05:39 +0300

    Fix: add missing dependencies and SubscriptionManager component

---
+++ components/billing/SubscriptionManager.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function SubscriptionManager() {
+  return (
+    <div className="p-4 border rounded">
+      <p>Subscription Manager placeholder</p>
+    </div>
+  );
+}
