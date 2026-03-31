acc0b7c15d7d4ca6ce0a934bcdb5310a9ada9e94
Usman Ashe <usmanashe686@gmail.com>
2026-03-31 07:28:16 +0300

    fix: convert DownloadPlugin to Java to resolve package visibility issues and active

--- android/app/src/main/java/com/aimetafactory/app/MainActivity.java
+++ android/app/src/main/java/com/aimetafactory/app/MainActivity.java
@@ -1,15 +1,5 @@
 package com.aimetafactory.app;
 
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
 
-public class MainActivity extends BridgeActivity {
-
-    @Override
-    public void onCreate(android.os.Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // 🔥 THIS IS THE CRITICAL FIX
-        registerPlugin(DownloadPlugin.class);
-    }
-}
+public class MainActivity extends BridgeActivity {}
